0cfd1a657b6f99055e7391d888b9ab96f7444da6
Bob Mannino <manninobobby@icloud.com>
2026-09-11 22:45:41 +0100

    refactor: make `main.rs` more rust idiomatic

--- src/main.rs
+++ src/main.rs
@@ -9,65 +9,58 @@ use std::process::ExitCode;
 
 use esp_idf_svc::hal::peripherals::Peripherals;
 
+use crate::{error::Result, wifi::Wifi};
+
 fn main() -> ExitCode {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let Ok(peripherals) = Peripherals::take() else {
-        log::error!("Failed to take peripherals");
-        return ExitCode::FAILURE;
-    };
-
-    let Ok(display) = tm1637::Tm1637::new(peripherals.pins.gpio18, peripherals.pins.gpio19) else {
-        log::error!("Failed to create TM1637 display");
-        return ExitCode::FAILURE;
-    };
-
-    let Ok(spinner) = display.spinner() else {
-        log::error!("Failed to create spinner");
-        return ExitCode::FAILURE;
-    };
-
-    let wifi = match (option_env!("WIFI_SSID"), option_env!("WIFI_PASSWORD")) {
-        (Some(ssid), Some(password)) => wifi::Wifi::new(peripherals.modem)
-            .and_then(|mut wifi| {
-                let ip = wifi.connect(ssid, password)?;
-                log::info!("Connected to {ssid} with IP {ip}");
-                Ok(wifi)
-            })
-            .inspect_err(|err| log::error!("Failed to connect to wifi: {err}"))
-            .ok(),
-        _ => None,
-    };
-
-    let mut hour: u8 = 0;
-    let mut minute: u8 = 0;
-
-    if let Some(wifi) = wifi {
-        let Ok(_) = wifi.ip() else {
-            log::error!("Failed to get IP");
-            return ExitCode::FAILURE;
-        };
-
-        let Ok(date) = time::sync().inspect_err(|err| log::error!("Failed to sync time: {err}")) else {
-            return ExitCode::FAILURE;
-        };
-
-        (hour, minute) = time::hour_minute(date);
-    } else if let Ok(date) = time::now() {
-        (hour, minute) = time::hour_minute(date);
+    match run() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(err) => {
+            log::error!("Firmware stopped: {err}");
+            ExitCode::FAILURE
+        }
     }
+}
 
+fn run() -> Result<()> {
+    let peripherals = Peripherals::take()?;
+
+    let display = tm1637::Tm1637::new(peripherals.pins.gpio18, peripherals.pins.gpio19)?;
+    let spinner = display.spinner()?;
+
+    let wifi = option_env!("WIFI_SSID")
+        .zip(option_env!("WIFI_PASSWORD"))
+        .and_then(|(ssid, password)| connect(peripherals.modem, ssid, password));
+
+    let (hour, minute) = match wifi {
+        // The radio has to stay alive until the time has been fetched.
+        Some(wifi) => {
+            wifi.ip()?;
+
+            time::hour_minute(time::sync()?)
+        }
+        None => time::now().map_or((0, 0), time::hour_minute),
+    };
     log::info!("Setting time to {hour}:{minute}");
-    let Ok(mut display) = spinner.stop() else {
-        log::error!("Failed to stop spinner");
-        return ExitCode::FAILURE;
-    };
 
-    let Ok(()) = display.time(hour, minute, true) else {
-        log::error!("Failed to set time on display");
-        return ExitCode::FAILURE;
-    };
+    let mut display = spinner.stop()?;
+    display.time(hour, minute, true)?;
 
-    ExitCode::SUCCESS
+    Ok(())
+}
+
+/// Joins the given network, logging rather than propagating a failure so the
+/// clock can still run offline.
+fn connect<'d>(modem: impl esp_idf_svc::hal::modem::WifiModemPeripheral + 'd, ssid: &str, password: &str) -> Option<Wifi<'d>> {
+    Wifi::new(modem)
+        .and_then(|mut wifi| {
+            let ip = wifi.connect(ssid, password)?;
+            log::info!("Connected to {ssid} with IP {ip}");
+
+            Ok(wifi)
+        })
+        .inspect_err(|err| log::error!("Failed to connect to wifi: {err}"))
+        .ok()
 }
