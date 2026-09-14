0b7a3f5376727718b3322b62885d6841db29b2e9
Bob Mannino <manninobobby@icloud.com>
2026-09-14 17:12:24 +0100

    empty struct

---
+++ src/ec11.rs
@@ -0,0 +1,9 @@
+use crate::error::Result;
+
+pub struct Ec11;
+
+impl Ec11 {
+    pub fn new() -> Result<Self> {
+        Ok(Self {})
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod ec11;
 mod error;
 mod http;
 mod speaker;
