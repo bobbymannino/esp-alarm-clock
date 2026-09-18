01f5a4c5fec67a31ea5b7e1fc46a5e7603c57319
Bob Mannino <manninobobby@icloud.com>
2026-09-18 17:25:59 +0100

    separate out app

---
+++ crates/gui/src/app.rs
@@ -0,0 +1,67 @@
+use gpui_kit::{
+    Size,
+    component::{button::*, *},
+    *,
+};
+
+pub struct MyApp;
+
+impl Render for MyApp {
+    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .v_flex()
+            .gap_2()
+            .size_full()
+            .items_center()
+            .justify_center()
+            .child("Hello, World!")
+            .child(
+                Button::new("ok")
+                    .primary()
+                    .label("Let's Go!")
+                    .on_click(|_, _, _| println!("Clicked!")),
+            )
+    }
+}
+
+fn window_options(cx: &mut App) -> WindowOptions {
+    WindowOptions {
+        window_min_size: Some(Size::new(Pixels::from(600.0), Pixels::from(600.0))),
+        titlebar: Some(TitlebarOptions {
+            title: Some(SharedString::from("ESP Alarm Clock")),
+            appears_transparent: false,
+            traffic_light_position: None,
+        }),
+        window_bounds: Some(WindowBounds::centered(Size::new(Pixels::from(600.0), Pixels::from(600.0)), cx)),
+        app_id: Some(String::from("esp-alarm-clock")),
+        ..Default::default()
+    }
+}
+
+pub fn run() {
+    let app = gpui_kit::application().with_assets(gpui_kit::assets::Assets);
+
+    app.run(move |cx| {
+        // This must be called before using any GPUI Component features.
+        gpui_kit::init(cx);
+
+        cx.on_window_closed(|cx, _| {
+            if cx.windows().is_empty() {
+                cx.quit();
+            }
+        })
+        .detach();
+
+        let window_options = window_options(cx);
+
+        cx.spawn(async move |cx| {
+            cx.open_window(window_options, |window, cx| {
+                let view = cx.new(|_| MyApp);
+                // This first level on the window, should be a Root.
+                cx.new(|cx| Root::new(view, window, cx))
+            })
+            .expect("Failed to open window");
+        })
+        .detach();
+    });
+}
