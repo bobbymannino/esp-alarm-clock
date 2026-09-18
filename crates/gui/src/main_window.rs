92460286ed2ba43a3247438fa412a5a0d1e081fa
Bob Mannino <manninobobby@icloud.com>
2026-09-18 18:09:44 +0100

    window ui

--- crates/gui/src/main_window.rs
+++ crates/gui/src/main_window.rs
@@ -1,12 +1,43 @@
 use gpui_kit::{
-    component::{button::*, label::Label, *},
+    component::{
+        ActiveTheme,
+        button::Button,
+        input::{Textarea, TextareaState},
+        label::Label,
+        scroll::ScrollableElement,
+    },
     *,
 };
 
 pub struct MainWindow;
 
 impl Render for MainWindow {
-    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        div().child(Label::new("ESP Alarm Clock"))
+    fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
+        let logs = cx.new(|cx| TextareaState::new(window, cx).placeholder("Logs"));
+        let theme = cx.theme();
+
+        div()
+            .overflow_y_scrollbar()
+            .flex()
+            .flex_col()
+            .p_5()
+            .gap_5()
+            .child(Label::new("ESP Alarm Clock").font_weight(FontWeight::BOLD).text_3xl())
+            .child(
+                Textarea::new(&logs)
+                    .h_96()
+                    .border_2()
+                    .border_color(theme.border)
+                    .rounded_lg()
+                    .readonly(true),
+            )
+            .child(
+                div().flex().child(
+                    Button::new("close")
+                        .outline()
+                        .label("Close")
+                        .on_click(|_, window, _| window.remove_window()),
+                ),
+            )
     }
 }
